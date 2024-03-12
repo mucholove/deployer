6ed97f8f30fa89e3784f819403a52cecf4fe0e1b
Gustavo Tavares <gustavo@froml.com>
2024-03-12 16:43:57 -0400

    .

--- deploy-libssh2.php
+++ deploy-libssh2.php
@@ -271,28 +271,3 @@ foreach ($commands as $command)
 }
 
 echo "Deployment script executed.\n";
-
-
-/*
-use phpseclib3\Net\SFTP;
-
-$localTempConfigPath = tempnam(sys_get_temp_dir(), 'apache_config_');
-
-if (file_put_contents($localTempConfigPath, $apacheConfigContent) === false) {
-    die("Failed to write local Apache configuration file.\n");
-}
-
-// Assuming $ssh is already authenticated
-$sftp = new SFTP($host, $port ?? 22);
-
-if (!$sftp->login($username, $password)) {
-    die('SFTP login failed');
-}
-
-if (!$sftp->put($apacheConfigFilePath, $localTempConfigPath, SFTP::SOURCE_LOCAL_FILE)) {
-    die("Failed to upload Apache configuration file to remote path: $apacheConfigFilePath\n");
-}
-
-// Delete the local temporary file after uploading
-unlink($localTempConfigPath);
-*/
--- deploy.php
+++ deploy.php
@@ -8,13 +8,6 @@ echo $autoloadFile."\n";
 
 require $autoloadFile;
 
-use phpseclib3\Net\SSH2;
-
-use function Deployer\error;
-use function PHPSTORM_META\map;
-
-SSH2::setDebugMode(true);
-
 if ($argc < 2) 
 {
     die("Usage: php script.php <server_name>\n");
@@ -277,28 +270,3 @@ foreach ($commands as $command)
 }
 
 echo "Deployment script executed.\n";
-
-
-/*
-use phpseclib3\Net\SFTP;
-
-$localTempConfigPath = tempnam(sys_get_temp_dir(), 'apache_config_');
-
-if (file_put_contents($localTempConfigPath, $apacheConfigContent) === false) {
-    die("Failed to write local Apache configuration file.\n");
-}
-
-// Assuming $ssh is already authenticated
-$sftp = new SFTP($host, $port ?? 22);
-
-if (!$sftp->login($username, $password)) {
-    die('SFTP login failed');
-}
-
-if (!$sftp->put($apacheConfigFilePath, $localTempConfigPath, SFTP::SOURCE_LOCAL_FILE)) {
-    die("Failed to upload Apache configuration file to remote path: $apacheConfigFilePath\n");
-}
-
-// Delete the local temporary file after uploading
-unlink($localTempConfigPath);
-*/
