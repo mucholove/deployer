85c58b1ce7086bb240f6ebdf112494459682028f
Gustavo Tavares <gustavo@froml.com>
2024-04-17 18:10:50 -0400

    .

--- deploy.php
+++ deploy.php
@@ -303,7 +303,8 @@ $symLinkCommand = null;
 
 if (isset($SERVER_CONFIG["canonicalPath"]))
 {
-    $symLinkCommand = new ScriptCommand('mklink /D "'.$SERVER_CONFIG["canonicalPath"].'" "'.$newFolderPath.'"');
+    
+    $symLinkCommand = new ScriptCommand('if exist "'.$SERVER_CONFIG["canonicalPath"].'" rmdir /s /q "'.$SERVER_CONFIG["canonicalPath"].' mklink /D "'.$SERVER_CONFIG["canonicalPath"].'" "'.$newFolderPath.'"');
 }
 
 $commands = [
